016cea2c01a9875bed860f81cbce443cbfc8c40c
Sebastian Piecha <sebastian@piecha.net>
2019-10-16 23:50:14 +0200

    Anpassungen für VScode

--- VS_Demo.ino
+++ VS_Demo.ino
@@ -4,12 +4,17 @@
     Author:     Sidey
 */
 
-
+// #include <Arduino.h>		// meistens in VScode und PlatformIO nötig
 #include <RunningAverage.h>
 
 RunningAverage avgCounter(5);
 void setup()
 {
+	Serial.begin(115200);
+	while (!Serial) {
+		delay(50);
+	}
+	delay(5000);
 	avgCounter.addValue(1);
 	avgCounter.addValue(2);
 	Serial.println(avgCounter.getAverage());
